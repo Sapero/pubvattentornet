416c6cb6232ac48c7acebdda049b09f8c2c0ccb8
Samuel Rothin <samuel.rothin@gmail.com>
2025-03-14 20:55:38 +0100

    added mera logo and link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import VattentornetDataService from '../services/vattentornet.service'
+import mera from '../images/mera.png'
 
 class Home extends Component {
   componentDidMount () {
@@ -18,6 +19,7 @@ class Home extends Component {
         <h1>FRE: 19-SENT</h1>
         <h2>(Men senast till 01!)</h2>
         <p>Lennings Gata 2</p>
+        <a href="https://mera.se/"><img src={mera} alt="Mera logo" /></a>
       </div>
     )
   }
